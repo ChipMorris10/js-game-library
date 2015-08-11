2f2ed68aaace11c7573025cb69b662f74da2cbc3
ChipMorris10 <goldcomac@mac.com>
2015-08-11 11:00:13 -0600

    added instances of the Game() class to the DOM

--- js/main.js
+++ js/main.js
@@ -2,4 +2,37 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
-});
+
+function Game(title, genre, year) {
+    this.title = title;
+    this.genre = genre;
+    this.year = year;
+}
+galaga = new Game('Galaga ', 'space ', 1985);
+console.log(galaga);
+
+Game.prototype.render = function(game) {
+    for (var x in game) {
+    $('#games').append(game[x]);
+    }
+
+};
+
+galaga.render(galaga);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // end of code
